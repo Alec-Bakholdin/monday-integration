f0c7ba5296e7bd6e4644c77c91d4024cdee4786c
Alec Bakholdin <alecbakholdin@gmail.com>
2021-12-28 18:30:04 -0500

    added some todos

--- src/aqua/model/WitreStylePO.cs
+++ src/aqua/model/WitreStylePO.cs
@@ -7,6 +7,10 @@ namespace monday_integration.src.aqua.model
         public string PurchaseOrderNo {get; set;}
         public string Vendor {get; set;}
 
+
+        //TODO: get rid of subitems and just do one line per JSON
+        //TODO: find some sort of VLOOKUP between boards for container delivery schedule and the big board
+        //TODO: move everything into ONE board
         [MondaySubitemColumnAttribute("text9")]public string Style {get; set;}
         [MondaySubitemColumnAttribute("text")]public string StyleDescription {get; set;}
         [MondaySubitemColumnAttribute("text2")]public string Color {get; set;}
